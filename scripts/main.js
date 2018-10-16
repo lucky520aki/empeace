cf0248e235d036729c780e62da34481de7501658
HAYASHI MUNEAKI <lucky520aki@yahoo.co.jp>
2018-10-17 01:13:15 +0900

    a

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,1 @@
-  // Closes the Responsive Menu on Menu Item Click
-  'click .navbar-collapse ul li a.toggle-navbar': function(event,template) {
-    // is the menu visible
-    if(template.$('#bs-example-navbar-collapse-1').is(':visible')) {
-      // click the toggle button
-      template.$('.menu-toggle:visible').click();
-    }
-  }
+$('.nav-item>li>a').on('click', function(){ $('.navbar-collapse').collapse('hide'); });</script>
