df49b817339f0ab4000c5227088548caaba5f79e
HAYASHI MUNEAKI <lucky520aki@yahoo.co.jp>
2018-10-17 17:49:20 +0900

    w

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,5 @@
-$('.nav-item>li>a').on('click', function(){ $('.navbar-collapse').collapse('hide'); });</script>
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a:not(".dropdown-toggle")') ) {
+        $(this).collapse('hide');
+    }
+});
